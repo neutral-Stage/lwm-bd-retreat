9cba445c837ff1230845de5856e4232a39ab4730
Shuvo Anirban Roy <codebuilderit@gmail.com>
2023-03-20 10:34:38 +0600

    present update

--- frontend/pages/list.js
+++ frontend/pages/list.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useRef } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -9,19 +9,37 @@ import TableRow from "@mui/material/TableRow";
 import { client } from "../service/sanityClient";
 import { fellowships } from "../data/fellowship";
 import { Divider, Typography } from "@mui/material";
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function StickyHeadTable(props) {
   const { participant } = props;
-  const male = participant.filter((p) => p.gender === "male");
-  const female = participant.filter((p) => p.gender === "female");
-  const getFellowship = fellowships.map((fel) => {
-    const getCount = participant.filter((p) => p.fellowshipName === fel);
+  const [participantState, setParticipantState] = useState(participant);
 
-    const getMale = participant.filter(
-      (p) => p.fellowshipName === fel && p.gender === "male"
+  const isPresent = (p) =>
+    p.present === "present" || p.present === null || p.present === undefined;
+  const male = participantState.filter(
+    (p) => p.gender === "male" && isPresent(p)
+  );
+  const female = participantState.filter(
+    (p) => p.gender === "female" && isPresent(p)
+  );
+  const getFellowship = fellowships.map((fel) => {
+    const getCount = participantState.filter(
+      (p) => p.fellowshipName === fel && isPresent(p)
     );
-    const getFemale = participant.filter(
-      (p) => p.fellowshipName === fel && p.gender === "female"
+
+    const getMale = participantState.filter(
+      (p) => p.fellowshipName === fel && p.gender === "male" && isPresent(p)
+    );
+    const getFemale = participantState.filter(
+      (p) => p.fellowshipName === fel && p.gender === "female" && isPresent(p)
     );
     return {
       fellowshipName: fel,
@@ -30,12 +48,76 @@ export default function StickyHeadTable(props) {
       female: getFemale.length,
     };
   });
-  const total = participant.length;
+  // const total = participantState.length;
+  const total = participantState.filter((p) => isPresent(p)).length;
 
-  const tableRef = React.useRef(null);
+  const tableRef = useRef(null);
+
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("");
+
+  const handleDialog = (value) => {
+    setSelected(value);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = async () => {
+    const upd_parti = participantState.map((obj) => {
+      if (selected._id === obj._id) {
+        obj.present = "absent";
+      }
+      return obj;
+    });
+
+    setParticipantState(upd_parti);
+    await client
+      .patch(selected._id) // Document ID to patch
+      .set({ present: "absent" }) // Increment field by count
+      .commit() // Perform the patch and return a promise
+      .then((updatedBike) => {
+        setOpen(false);
+        setSelected("");
+        console.log("Hurray, the participant is updated! New document:");
+        console.log(updatedBike);
+      })
+      .catch((err) => {
+        console.error("Oh no, the update failed: ", err.message);
+      });
+  };
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {`Are you sure you want to delete  ?`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {selected?.name} from {selected?.fellowshipName}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="primary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={handleDelete}
+            autoFocus
+          >
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
       <TableContainer
         component={Paper}
         sx={{
@@ -108,8 +190,12 @@ export default function StickyHeadTable(props) {
       </TableContainer>
       <Divider sx={{ mt: "2rem" }} />
       {fellowships.map((fel) => {
-        const participantsByFel = participant.filter(
-          (p) => p.fellowshipName === fel
+        const participantsByFel = participantState.filter(
+          (p) =>
+            p.fellowshipName === fel &&
+            (p.present === "present" ||
+              p.present === null ||
+              p.present === undefined)
         );
         if (participantsByFel.length === 0) {
           return;
@@ -140,6 +226,7 @@ export default function StickyHeadTable(props) {
                   <TableCell> Name</TableCell>
                   <TableCell align="right">Contact</TableCell>
                   <TableCell align="right">Gender</TableCell>
+                  <TableCell align="right">action</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -153,6 +240,14 @@ export default function StickyHeadTable(props) {
                     </TableCell>
                     <TableCell align="right">{row.contact}</TableCell>
                     <TableCell align="right">{row.gender}</TableCell>
+                    <TableCell align="right">
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => handleDialog(row)}
+                      >
+                        <DeleteForeverOutlinedIcon color="error" />
+                      </IconButton>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
@@ -168,7 +263,7 @@ export async function getStaticProps() {
   // It's important to default the slug so that it doesn't return "undefined"
   // const { slug = "" } = context.params
   const participant = await client.fetch(
-    '*[_type == "participant"]| order(_createdAt desc)'
+    '*[_type == "participant" && (present == "present" || present == null) ]| order(_createdAt desc)'
   );
   return {
     props: {
