5ec3f3823393f999d247044ce1d40cf2bb51c865
Shuvo Anirban Roy <anirbanroy691@gmail.com>
2023-10-03 12:25:51 +0600

    update for dhaka retreat list

--- frontend/pages/list.js
+++ frontend/pages/list.js
@@ -1,110 +1,110 @@
-import { useState, useRef } from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { client } from "../service/sanityClient";
-import { fellowships } from "../data/fellowship";
-import { Divider, Typography } from "@mui/material";
-import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
-import EditIcon from "@mui/icons-material/Edit";
-import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import EditModalForm from "../components/EditModalForm";
+import { useState, useRef } from 'react'
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import { client } from '../service/sanityClient'
+import { dhakaRetreatFellowships } from '../data/fellowship'
+import { Divider, Typography } from '@mui/material'
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined'
+import EditIcon from '@mui/icons-material/Edit'
+import IconButton from '@mui/material/IconButton'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import EditModalForm from '../components/EditModalForm'
 
 export default function StickyHeadTable(props) {
-  const { participant } = props;
-  const [participantState, setParticipantState] = useState(participant);
+  const { participant } = props
+  const [participantState, setParticipantState] = useState(participant)
 
   const isPresent = (p) =>
-    p.present === "present" || p.present === null || p.present === undefined;
+    p.present === 'present' || p.present === null || p.present === undefined
   const male = participantState.filter(
-    (p) => p.gender === "male" && isPresent(p)
-  );
+    (p) => p.gender === 'male' && isPresent(p)
+  )
   const female = participantState.filter(
-    (p) => p.gender === "female" && isPresent(p)
-  );
-  const getFellowship = fellowships.map((fel) => {
+    (p) => p.gender === 'female' && isPresent(p)
+  )
+  const getFellowship = dhakaRetreatFellowships.map((fel) => {
     const getCount = participantState.filter(
       (p) => p.fellowshipName === fel && isPresent(p)
-    );
+    )
 
     const getMale = participantState.filter(
-      (p) => p.fellowshipName === fel && p.gender === "male" && isPresent(p)
-    );
+      (p) => p.fellowshipName === fel && p.gender === 'male' && isPresent(p)
+    )
     const getFemale = participantState.filter(
-      (p) => p.fellowshipName === fel && p.gender === "female" && isPresent(p)
-    );
+      (p) => p.fellowshipName === fel && p.gender === 'female' && isPresent(p)
+    )
     return {
       fellowshipName: fel,
       count: getCount.length,
       male: getMale.length,
       female: getFemale.length,
-    };
-  });
+    }
+  })
   // const total = participantState.length;
-  const total = participantState.filter((p) => isPresent(p)).length;
+  const total = participantState.filter((p) => isPresent(p)).length
 
-  const tableRef = useRef(null);
+  const tableRef = useRef(null)
 
-  const [open, setOpen] = useState(false);
-  const [editOpen, setEditOpen] = useState(false);
-  const [selected, setSelected] = useState("");
+  const [open, setOpen] = useState(false)
+  const [editOpen, setEditOpen] = useState(false)
+  const [selected, setSelected] = useState('')
 
   const handleDialog = (value) => {
-    setSelected(value);
-    setOpen(true);
-  };
+    setSelected(value)
+    setOpen(true)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
   const handleEditDialog = (value) => {
-    setSelected(value);
-    setEditOpen(true);
-  };
+    setSelected(value)
+    setEditOpen(true)
+  }
 
   const handleEditClose = () => {
-    setEditOpen(false);
-  };
+    setEditOpen(false)
+  }
 
   const handleDelete = async () => {
     const upd_parti = participantState.map((obj) => {
       if (selected._id === obj._id) {
-        obj.present = "absent";
+        obj.present = 'absent'
       }
-      return obj;
-    });
-    setOpen(false);
-    setParticipantState(upd_parti);
+      return obj
+    })
+    setOpen(false)
+    setParticipantState(upd_parti)
     await client
       .delete(selected._id)
       .then((updatedBike) => {
-        setSelected("");
-        console.log("Hurray, the participant is updated! New document:");
-        console.log(updatedBike);
+        setSelected('')
+        console.log('Hurray, the participant is updated! New document:')
+        console.log(updatedBike)
       })
       .catch((err) => {
-        console.error("Oh no, the update failed: ", err.message);
-      });
-  };
+        console.error('Oh no, the update failed: ', err.message)
+      })
+  }
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
       <Dialog
         open={editOpen}
         onClose={handleEditClose}
-        aria-labelledby="edit-participant"
+        aria-labelledby='edit-participant'
       >
-        <DialogTitle id="edit-participant">
+        <DialogTitle id='edit-participant'>
           {`Edit ${selected?.name} from ${selected?.fellowshipName}`}
         </DialogTitle>
         <DialogContent>
@@ -119,7 +119,7 @@ export default function StickyHeadTable(props) {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button variant="contained" color="primary" onClick={handleEditClose}>
+          <Button variant='contained' color='primary' onClick={handleEditClose}>
             Close
           </Button>
         </DialogActions>
@@ -127,9 +127,9 @@ export default function StickyHeadTable(props) {
       <Dialog
         open={open}
         onClose={handleClose}
-        aria-labelledby="delete-participant"
+        aria-labelledby='delete-participant'
       >
-        <DialogTitle id="delete-participant">
+        <DialogTitle id='delete-participant'>
           {`Are you sure you want to delete  ?`}
         </DialogTitle>
         <DialogContent>
@@ -138,12 +138,12 @@ export default function StickyHeadTable(props) {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button variant="contained" color="primary" onClick={handleClose}>
+          <Button variant='contained' color='primary' onClick={handleClose}>
             Close
           </Button>
           <Button
-            variant="contained"
-            color="error"
+            variant='contained'
+            color='error'
             onClick={handleDelete}
             autoFocus
           >
@@ -155,65 +155,65 @@ export default function StickyHeadTable(props) {
         component={Paper}
         sx={{
           p: 4,
-          maxWidth: "60rem",
+          maxWidth: '60rem',
           my: 4,
-          mx: "auto",
-          boxShadow: "0px 0px 8px 8px rgba(0, 0, 0,0.2)",
+          mx: 'auto',
+          boxShadow: '0px 0px 8px 8px rgba(0, 0, 0,0.2)',
         }}
       >
-        <Table aria-label="sticky table" size="small">
+        <Table aria-label='sticky table' size='small'>
           <TableHead>
             <TableRow>
               <TableCell>Fellowship Name</TableCell>
-              <TableCell align="center">Male</TableCell>
-              <TableCell align="center">Female</TableCell>
-              <TableCell align="center">Participants</TableCell>
+              <TableCell align='center'>Male</TableCell>
+              <TableCell align='center'>Female</TableCell>
+              <TableCell align='center'>Participants</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {getFellowship.map((row, index) => (
               <TableRow
                 key={index}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
+                <TableCell component='th' scope='row'>
                   {row.fellowshipName}
                 </TableCell>
-                <TableCell align="center">{row.male}</TableCell>
-                <TableCell align="center">{row.female}</TableCell>
-                <TableCell align="center">{row.count}</TableCell>
+                <TableCell align='center'>{row.male}</TableCell>
+                <TableCell align='center'>{row.female}</TableCell>
+                <TableCell align='center'>{row.count}</TableCell>
               </TableRow>
             ))}
 
             <TableRow
-              key="akjsdhnaksdnla-aslkjdjalsd"
+              key='akjsdhnaksdnla-aslkjdjalsd'
               sx={{
-                "&:last-child td, &:last-child th": { border: 0 },
-                fontWeight: "bold",
+                '&:last-child td, &:last-child th': { border: 0 },
+                fontWeight: 'bold',
               }}
             >
               <TableCell
-                component="th"
-                scope="row"
-                sx={{ fontWeight: "bold", fontSize: "1.5rem" }}
+                component='th'
+                scope='row'
+                sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
               >
                 Total
               </TableCell>
               <TableCell
-                align="center"
-                sx={{ fontWeight: "bold", fontSize: "1.5rem" }}
+                align='center'
+                sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
               >
                 {male.length}
               </TableCell>
               <TableCell
-                align="center"
-                sx={{ fontWeight: "bold", fontSize: "1.5rem" }}
+                align='center'
+                sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
               >
                 {female.length}
               </TableCell>
               <TableCell
-                align="center"
-                sx={{ fontWeight: "bold", fontSize: "1.5rem" }}
+                align='center'
+                sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}
               >
                 {total}
               </TableCell>
@@ -221,85 +221,85 @@ export default function StickyHeadTable(props) {
           </TableBody>
         </Table>
       </TableContainer>
-      <Divider sx={{ mt: "2rem" }} />
-      {fellowships.map((fel) => {
+      <Divider sx={{ mt: '2rem' }} />
+      {dhakaRetreatFellowships.map((fel) => {
         const participantsByFel = participantState.filter(
           (p) =>
             p.fellowshipName === fel &&
-            (p.present === "present" ||
+            (p.present === 'present' ||
               p.present === null ||
               p.present === undefined)
-        );
+        )
         if (participantsByFel.length === 0) {
-          return;
+          return
         }
-        var now = new Date();
-        var current_year = now.getFullYear();
+        var now = new Date()
+        var current_year = now.getFullYear()
         return (
           <TableContainer
             key={fel}
             component={Paper}
             sx={{
               p: 4,
-              maxWidth: "50rem",
+              maxWidth: '50rem',
               my: 4,
-              mx: "auto",
-              boxShadow: "0px 0px 8px 8px rgba(0, 0, 0,0.2)",
+              mx: 'auto',
+              boxShadow: '0px 0px 8px 8px rgba(0, 0, 0,0.2)',
             }}
           >
-            <Typography variant="h6" sx={{ textDecoration: "underline" }}>
+            <Typography variant='h6' sx={{ textDecoration: 'underline' }}>
               {fel}
             </Typography>
             <Table
-              aria-label="sticky table"
-              size="small"
+              aria-label='sticky table'
+              size='small'
               ref={tableRef}
               sx={{ p: 4 }}
             >
               <TableHead>
                 <TableRow>
                   <TableCell> Name</TableCell>
-                  <TableCell align="right">Contact</TableCell>
-                  <TableCell align="right">Gender</TableCell>
-                  <TableCell align="right">Department</TableCell>
-                  <TableCell align="right">Age</TableCell>
-                  <TableCell align="right">Saved/Unsaved</TableCell>
-                  <TableCell align="right">Edit</TableCell>
-                  <TableCell align="right">Delete</TableCell>
+                  <TableCell align='right'>Contact</TableCell>
+                  <TableCell align='right'>Gender</TableCell>
+                  <TableCell align='right'>Department</TableCell>
+                  <TableCell align='right'>Age</TableCell>
+                  <TableCell align='right'>Saved/Unsaved</TableCell>
+                  <TableCell align='right'>Edit</TableCell>
+                  <TableCell align='right'>Delete</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {participantsByFel.map((row) => (
                   <TableRow
                     key={row._id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   >
-                    <TableCell component="th" scope="row">
+                    <TableCell component='th' scope='row'>
                       {row.name}
                     </TableCell>
-                    <TableCell align="right">{row.contact}</TableCell>
-                    <TableCell align="right">{row.gender}</TableCell>
-                    <TableCell align="right">{row.department}</TableCell>
-                    <TableCell align="right">
+                    <TableCell align='right'>{row.contact}</TableCell>
+                    <TableCell align='right'>{row.gender}</TableCell>
+                    <TableCell align='right'>{row.department}</TableCell>
+                    <TableCell align='right'>
                       {current_year - row.birthYear}
                     </TableCell>
-                    <TableCell align="center">
-                      {row.isSaved ? "Saved" : "Unsaved"}
+                    <TableCell align='center'>
+                      {row.isSaved ? 'Saved' : 'Unsaved'}
                     </TableCell>
                     <TableCell>
                       <IconButton
-                        aria-label="edit"
+                        aria-label='edit'
                         onClick={() => handleEditDialog(row)}
                       >
-                        <EditIcon color="primary" />
+                        <EditIcon color='primary' />
                       </IconButton>
                     </TableCell>
                     <TableCell>
                       <IconButton
-                        aria-label="delete"
+                        aria-label='delete'
                         onClick={() => handleDialog(row)}
                       >
-                        <DeleteForeverOutlinedIcon color="error" />
+                        <DeleteForeverOutlinedIcon color='error' />
                       </IconButton>
                     </TableCell>
                   </TableRow>
@@ -307,22 +307,22 @@ export default function StickyHeadTable(props) {
               </TableBody>
             </Table>
           </TableContainer>
-        );
+        )
       })}
     </Paper>
-  );
+  )
 }
 
 export async function getStaticProps() {
   // It's important to default the slug so that it doesn't return "undefined"
   // const { slug = "" } = context.params
   const participant = await client.fetch(
-    '*[_type == "participant" && (present == "present" || present == null) ]| order(_createdAt desc){..., "imgUrl": image.asset->url}'
-  );
+    '*[_type == "participant" && (present == "present" || present == null) && !(fellowshipName in ["Bandarban Fellowship", "Hativanga fellowship", "Muladoli Fellowship", "Ruma Fellowship", "Sinaipara Fellowship","Vaggomonipara fellowship"]) ]| order(_createdAt desc){..., "imgUrl": image.asset->url}'
+  )
   return {
     props: {
       participant,
     },
     revalidate: 10,
-  };
+  }
 }
